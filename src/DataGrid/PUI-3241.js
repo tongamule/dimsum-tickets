b328c901e4eb4ae71d73246ed9c6de5cfd7b62c9
benjamin.kugler <benjamin.kugler@elliemae.com>
2020-06-30 19:13:17 -0300

    PUI-3241: Added custom renderer for dropdown options

--- src/DataGrid/PUI-3241.js
+++ src/DataGrid/PUI-3241.js
@@ -5,6 +5,7 @@ import DSDropdownMenu from '@elliemae/ds-dropdownmenu';
 import DSButton from '@elliemae/ds-basic/Button';
 import PhoneOutline from '@elliemae/ds-icons/PhoneOutline';
 import EditPencil from '@elliemae/ds-icons/EditPencil';
+import DSTooltip from '@elliemae/ds-basic/Tooltip';
 import editableColumns from './editableColumns';
 import { randomEntities } from '../utils/randomData';
 
@@ -14,6 +15,16 @@ const options = [
   {
     id: '1',
     label: 'option 1',
+    customRenderer: (
+      <DSTooltip
+        triggerComponent={
+          <DSButton labelText="Custom render" buttonType="link" />
+        }
+        interactionType="hover"
+        title="Click to copy"
+        zIndex={1000}
+      />
+    ),
   },
   {
     id: '2',
@@ -45,13 +56,13 @@ export default () => {
             icon={
               <DSDropdownMenu
                 options={options}
+                type="phone"
                 triggerComponent={
                   <DSButton
                     buttonType="link"
                     icon={<PhoneOutline />}
                   />
                 }
-                zIndex={1000}
               />
             }
           />
